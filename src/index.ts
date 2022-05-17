cf25cca95d6149458bae9038f5836f4a8bc5ea72
oakir <oakir@ecomundo.eu>
2022-05-17 03:57:15 +0200

    start the apollo server express

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import WildersController from './controllers/Wilders';
+import { typeDefs } from './gql/schema';
+import { resolvers } from './gql/resolvers';
+import { startApolloServer } from './startApolloServer';
 
 
 mongoose.connect('mongodb://localhost:27017/wildapi2', {
@@ -14,3 +17,6 @@ mongoose.connect('mongodb://localhost:27017/wildapi2', {
     });
 
 
+startApolloServer(typeDefs, resolvers).then(() => {
+ console.log(' Server ready at http://localhost:4000/graphql 🚀🚀🚀');   
+});
