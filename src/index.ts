26149858e5abbd6d2df27fcf333a95dc6e476431
oakir <oakir@ecomundo.eu>
2022-05-17 03:50:34 +0200

    clean index with only needed code

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,6 @@ import express from 'express';
 import mongoose from 'mongoose';
 import WildersController from './controllers/Wilders';
 
-const { ApolloServer, gql } = require('apollo-server-express');
-
-const app = express();
-
 
 mongoose.connect('mongodb://localhost:27017/wildapi2', {
     autoIndex: true
@@ -17,8 +13,4 @@ mongoose.connect('mongodb://localhost:27017/wildapi2', {
         console.log("Not connected");
     });
 
-
-app.listen(4000, function () {
-    console.log("Server started on port 4000!");
-});
 
