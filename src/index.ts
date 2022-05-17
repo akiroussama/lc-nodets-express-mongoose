1982eb628b44f8e13b951ac3babbc73ddce8732e
oakir <oakir@ecomundo.eu>
2022-05-17 13:09:52 +0200

    test backend apollo server

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import WildersController from './controllers/Wilders';
-import { typeDefs } from './gql/schema';
-import { resolvers } from './gql/resolvers';
-import { startApolloServer } from './startApolloServer';
-import { connectDatabase } from './connectDataBase';
+import express from "express";
+import mongoose from "mongoose";
+import WildersController from "./controllers/Wilders";
+import { typeDefs } from "./gql/schema";
+import { resolvers } from "./gql/resolvers";
+import { connectDatabase } from "./connectDataBase";
+import startApolloServer from "./startApolloServer";
 
 connectDatabase();
 startApolloServer(typeDefs, resolvers).then(() => {
- console.log(' Server ready at http://localhost:4000/graphql 🚀🚀🚀');   
+	console.log(" Server ready at http://localhost:4000/graphql 🚀🚀🚀");
 });
--- src/startApolloServer.ts
+++ src/startApolloServer.ts
@@ -1,13 +1,16 @@
 import express from "express";
-import {ApolloServer} from "apollo-server-express";
+import { ApolloServer } from "apollo-server-express";
 
-export async  function startApolloServer(typeDefs: any, resolvers: any) {
-    const app = express();
-    const server = new ApolloServer({
-        typeDefs,
-        resolvers,
-    });
-    await server.start();
-    server.applyMiddleware({ app });
-    app.listen({ port: 4000 });
+async function startApolloServer(typeDefs: any, resolvers: any) {
+	const app = express();
+	const server = new ApolloServer({
+		typeDefs,
+		resolvers,
+	});
+	await server.start();
+	server.applyMiddleware({ app });
+	app.listen({ port: 4000 });
+	return app;
 }
+
+export default startApolloServer;
